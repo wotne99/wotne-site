8d3e288f9bd91be37689974d9156c537b1869c47
wotne99 <armyyusuf@hotmail.com>
2025-08-02 11:09:50 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,11 +1,1 @@
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Wotne from './Wotne'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Wotne />
-  </React.StrictMode>
-)
